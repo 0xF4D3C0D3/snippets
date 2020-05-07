5c6b30d5d0c972951b4b76ef078e0ba35397c97e
Jung Dong Ho <dongho971220@gmail.com>
2020-05-07 22:39:53 +0900

    Create 1110_addition_cycle.py

---
+++ boj/1110_addition_cycle.py
@@ -0,0 +1,10 @@
+start = f'{input():>02}'
+
+get_next = lambda x: x + str(sum(map(int, start))%10)
+next_ = get_next(start[1])
+
+count = 1
+while next_ != start:
+    count += 1
+    next_ = get_next(next[1])
+print(count)
