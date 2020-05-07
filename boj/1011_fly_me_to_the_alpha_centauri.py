5ef64b638e893f9b43f9eff45ae80df8e9f40f46
Jung Dong Ho <dongho971220@gmail.com>
2020-05-07 13:47:22 +0900

    Create 1011_fly_me_to_the_alpha_centauri.py

---
+++ boj/1011_fly_me_to_the_alpha_centauri.py
@@ -0,0 +1,11 @@
+def solve(d):
+	sqrt_d = round(d**0.5)
+	if sqrt_d**2 - sqrt_d + 1 <= d <= sqrt_d**2:
+		return sqrt_d*2-1
+	else:
+		return sqrt_d*2
+
+T = int(input())
+for _ in range(T):
+    start, end = map(int, input().split())
+    print(solve(end - start))
