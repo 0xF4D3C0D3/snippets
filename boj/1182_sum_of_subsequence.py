c1b0e074d687806fc2c7dbd4684a54df9e0bc34b
Jung Dong Ho <dongho971220@gmail.com>
2020-05-09 14:04:03 +0900

    Update 1182_sum_of_subsequence.py

--- boj/1182_sum_of_subsequence.py
+++ boj/1182_sum_of_subsequence.py
@@ -1,16 +1,14 @@
+# https://github.com/0xF4D3C0D3/snippets/blob/master/boj/1182_sum_of_subsequence.py
+
 from itertools import chain, combinations
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    "powerset([1,2,3]) --> (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
 
 N, S = map(int, input().split())
 L = list(map(int, input().split()))
 
-count = 0
-for sub in list(powerset(L))[1:]:
-    if sum(sub) == S:
-        count +=1
-
+count = sum(sum(sub) == S for sub in powerset(L))
 print(count)
