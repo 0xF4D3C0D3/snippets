b196061d7380b9cced29774024c37172b9137433
Jung Dong Ho <dongho971220@gmail.com>
2020-05-09 18:45:36 +0900

    Update 1193_finding_fraction.py

--- boj/1193_finding_fraction.py
+++ boj/1193_finding_fraction.py
@@ -1,10 +1,12 @@
+# https://github.com/0xF4D3C0D3/snippets/blob/master/boj/1193_finding_fraction.py
+
 """
 # Overview
 
 1/1	1/2	1/3	1/4	1/5	…
 2/1	2/2	2/3	2/4	…  	…
 3/1	3/2	3/3	…  	…  	…
-4/1	4/2	…       …  	…  	…
+4/1	4/2	…   …  	…  	…
 5/1	…	…  	…	…  	…
 …	… 	…	…  	…  	…
 Above is infinite sized matrix enumerating fractions in zigzag order like below
@@ -15,12 +17,12 @@ So how can we find the x-th fraction when x is given?
 
 # Approach
 Let's rotate it 45 degree and it would looks like below
-        1/1         ... 1
-      1/2 2/1       ... 2
-    3/1 2/2 1/3     ... 3
-  1/4 2/3 3/2 1/4   ... 4
-5/1 4/2 3/3 2/4 1/5 ... 5
-As you can see the odd floors start from n/1, whereas even floors start from 1/n
+        1/1         ... 0
+      1/2 2/1       ... 1
+    3/1 2/2 1/3     ... 2
+  1/4 2/3 3/2 1/4   ... 3
+5/1 4/2 3/3 2/4 1/5 ... 4
+As you can see the odd floors start from 1/n, whereas even floors start from n/1
 and there are n fractions in the n-th floor so we can make a general term like below
 the index of start at n-th floor = 1 + n(n+1)//2  ∵ it's difference sequence and its common difference is 1 and first term is 1
 
@@ -28,6 +30,18 @@ the index of start at n-th floor = 1 + n(n+1)//2  ∵ it's difference sequence a
 1. Find the maximum n such that n(n+1) <= 2(x-1)
 2. Find the offset with |x - (1 + n(n+1)//2)|
 3. Let's a = n-offset, b = 1+offset
-4. Print b/a if n is even number else a/b
+4. Print a/b if n is even number else b/a
 """
 
+def find_n(x):
+    n = int((2*(x-1))**0.5)
+    while n*(n+1) > 2*(x-1):
+        n -= 1
+    return n
+
+x = int(input())
+n = find_n(x)
+offset = abs(x - (1 + n*(n+1)//2))
+a = 1+n-offset
+b = 1+offset
+print(f'{a}/{b}' if n%2==0 else f'{b}/{a}')
