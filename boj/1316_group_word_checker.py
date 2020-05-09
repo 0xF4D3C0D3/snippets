093c13a1f5289ae7e5c0c5f4f4fcdaa8133fd1c9
Jung Dong Ho <dongho971220@gmail.com>
2020-05-10 00:13:35 +0900

    Update 1316_group_word_checker.py

--- boj/1316_group_word_checker.py
+++ boj/1316_group_word_checker.py
@@ -1,18 +1,11 @@
+# https://github.com/0xF4D3C0D3/snippets/edit/master/boj/1316_group_word_checker.py
+
 def is_group(s):
     prev_chars = set()
-
-    prev = ''
-    for c in s:
-        if c in prev_chars:
-            if prev != c:
-                return False
-        else:
+    for c, lead in zip(s, s[1:]+'_'):
+        if c != lead:
+            if c in prev_chars: return False
             prev_chars.add(c)
-        prev = c
     return True
 
-res = 0
-for _ in range(int(input())):
-    res += int(is_group(input()))
-    
-print(res)
+print(sum(is_group(input()) for _ in range(int(input()))))
