d4db5f9beccc3f50f60a57db3f3f63d9a0362152
Jung Dong Ho <dongho971220@gmail.com>
2020-05-09 22:12:46 +0900

    Update powerset.py

--- algorithm/powerset.py
+++ algorithm/powerset.py
@@ -1,6 +1,6 @@
-from itertools import chain, combinations
+import itertools
 
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
