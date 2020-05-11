f410a6023d8332660d72fb5d1c68437e74be164a
Jung Dong Ho <dongho971220@gmail.com>
2020-05-11 13:52:38 +0900

    Update bisect.py

--- algorithm/bisect.py
+++ algorithm/bisect.py
@@ -3,19 +3,19 @@ a = [1, 2, 3, 5, 5, 9, 9, 9, 9, 9, 15]
 pred_l = lambda i: a[i] < 9
 pred_r = lambda i: a[i] > 9
 
-def binary_search_left(L, R, pred):
-    while L < R:
-        m = (L+R)//2
-        if pred(m): L = m+1
-        else: R = m
-    return L
+def binary_search_left(l, r, pred):
+    while l < r:
+        m = (l+r)//2
+        if pred(m): l=m+1
+        else: r=m
+    return l
 
-def binary_search_right(L, R, pred):
-    while L < R:
-        m = (L+R)//2
-        if pred(m): R = m
-        else: L = m+1
-    return R-1
+def binary_search_right(l, r, pred):
+    while l < r:
+        m = (l+r)//2
+        if pred(m): r=m
+        else: l=m+1
+    return r-1
 
 
 print(binary_search_left(0, 10, pred_l), binary_search_right(0, 10, pred_r))
