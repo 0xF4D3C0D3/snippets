ffd66b04fdd7c5ce229d029250532b6088e171eb
Jung Dong Ho <dongho971220@gmail.com>
2020-05-11 14:00:05 +0900

    Update 1654_cutting_the_lan.py

--- ps/boj/py/1654_cutting_the_lan.py
+++ ps/boj/py/1654_cutting_the_lan.py
@@ -1,21 +1,19 @@
 import sys
 
-def binary_search_left(l, r, pred):
+def binary_search_right(l, r, pred):
     while l < r:
         m = (l+r)//2
-        if pred(m): l=m+1
-        else: r=m
-    return l
+        if pred(m): r=m
+        else: l=m+1
+    return r-1
 
 def get_how_many_lans_with_x_length(lans, x):
-    return sum([l//x for l in lans])
+    return sum(l//x for l in lans)
 
 def get_max_length(lans, N):
     def is_less_than_N_with_x_length(x):
         return get_how_many_lans_with_x_length(lans, x) < N
-    answer = binary_search_left(1, max(lans), is_less_than_N_with_x_length)
-    while get_how_many_lans_with_x_length(lans, answer) < N:
-        answer -= 1
+    answer = binary_search_right(1, max(lans), is_less_than_N_with_x_length)
     return answer
 
 lines = sys.stdin.readlines()
